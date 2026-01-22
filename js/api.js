ace2ec0ee4b5f012b04ab857af555026195bfc92
op271 <op271@fif.co.id>
2026-01-22 11:37:02 +0700

    Update API BASE URL

--- js/api.js
+++ js/api.js
@@ -8,8 +8,12 @@
  * Modul ini menangani semua komunikasi dengan backend API.
  */
 
-const API_BASE_URL = 'http://localhost:8000/api/v1';
-const HEALTH_URL = 'http://localhost:8000/health';
+const API_BASE_URL = '/api/v1';
+const HEALTH_URL = '/health';
+
+// Untuk Local
+// const API_BASE_URL = 'http://129.212.228.132:8000/api/v1';
+// const HEALTH_URL = 'http://129.212.228.132:8000/health';
 
 /**
  * API Client Class
